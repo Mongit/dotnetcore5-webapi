496da7d5246274a906846600280f96b605945946
Monserrat <monserrat.jimenez2@gmail.com>
2022-08-31 15:42:29 -0500

    ConnectionString and Adding th DBContext

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -1,6 +1,15 @@
-﻿namespace dotnet5_webapi.Data
+﻿using dotnet5_webapi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnet5_webapi.Data
 {
-    public class DataContext
+    public class DataContext : DbContext
     {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,8 @@
+using dotnet5_webapi.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,7 +27,7 @@ namespace dotnet5_webapi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<DataContext>(x => x.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
